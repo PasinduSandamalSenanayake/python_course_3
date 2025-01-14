eb06b85427def42999e7bcd49b62e48ff742ee3f
Pasindu Sandamal Senanayake <138322414+PasinduSandamalSenanayake@users.noreply.github.com>
2025-01-14 07:52:12 +0530

    Day - 45 Done

---
+++ day - 45.1/main.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup # Beautiful soup is a library for pulling data out of HTML files.
+
+# with open("website.html") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser") # html.website helps to contents
+#
+# # How to access website.html data using code.
+# # print(soup.title) Get the title of the web page, .a , .p , .h2 when use those things that can get the related things in here.
+# # print(soup.prettify())  # Full code with proper structure.
+#
+#
+# #  Finds in website
+# all_anchor_tags = soup.find_all(name="a") # a is links in the web
+#
+# for tag in all_anchor_tags:
+#     print(tag.getText())  # getText() - get only text of the tag
+#
+# unique_data = soup.find_all(name="h1", id="name")
+# print(unique_data)
+
+
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+# print(yc_web_page)
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+article_tag = soup.find(name="a", class_="storylink")
+print(article_tag)
+
+
+
